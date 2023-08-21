c65e7aa1ca488da36712c29479ded1e3381d79c8
Timothy Moore <mtimothy984@gmail.com>
2023-08-21 11:33:54 -0700

    Fix "npx not found"

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-. /home/ec2-user/config.sh
+source /root/.bashrc
+source /root/.nvm/nvm.sh
+source /home/ec2-user/config.sh
 npx ts-node --experimental-specifier-resolution=node --esm src/index.ts --host 0.0.0.0 --port 80
